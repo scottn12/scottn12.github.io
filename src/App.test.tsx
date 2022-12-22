5a03569173d6bf8083d22dc7ad21f304e58c56d1
Scott Norton <scott.norton12@gmail.com>
2022-12-22 16:54:40 -0500

    add test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders loading message', () => {
   render(<App />);
   const linkElement = screen.getByText(/Loading.../i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('renders leaderboard', async () => {
+  render(<App />);
+  await waitFor(() => {
+    const linkElement = screen.getByText(/Epic Ranked Momes!/i);
+    expect(linkElement).toBeInTheDocument();
+  });
 });
